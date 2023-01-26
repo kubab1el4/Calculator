106fb7ea0e8eb822cf4e45a9998139217fa1009b
Jakub Bielański <gg18051805@gmail.com>
2023-01-26 19:55:25 +0100

    Added fetch method to Currency Controller

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -1,10 +1,26 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Currency;
+use Illuminate\Support\Facades\Http;
 
 use Illuminate\Http\Request;
 
 class CurrencyController extends Controller
 {
-    //
+    public function fetch(){
+        $response = Http::get('http://api.nbp.pl/api/exchangerates/tables/A/');
+        $tables= json_decode($response->body());
+        $table= $tables[0];
+        foreach($table->rates as $data){
+            $currency = new Currency;
+            $currency->name = $data->currency;
+            $currency->exchange_rate = $data->mid;
+            $currency->currency_code = $data->code;
+            if(empty($record = Currency::where('currency_code', $currency->currency_code))){
+            $currency->save();}
+            else{$record->update(['exchange_rate'=>$currency->exchange_rate]);}
+        }
+        return "DONE";
+    }
 }
