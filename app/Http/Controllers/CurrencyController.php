4b97418e7e678b25e42c4e1fa2a066c5642a8302
Jakub Bielański <gg18051805@gmail.com>
2023-01-26 20:45:22 +0100

    Added error handling, final commit

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 use App\Models\Currency;
 use Illuminate\Support\Facades\Http;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 use Illuminate\Http\Request;
 
 class CurrencyController extends Controller
 {
     public function fetch(){
+        try {
         $response = Http::get('http://api.nbp.pl/api/exchangerates/tables/A/');
         $tables= json_decode($response->body());
         $table= $tables[0];
@@ -18,11 +20,16 @@ class CurrencyController extends Controller
             $currency->exchange_rate = $data->mid;
             $currency->currency_code = $data->code;
             $record=Currency::where('currency_code', $currency->currency_code);
-            if($record->exists()){
+            if($record->exists()) {
                 $record->update(['exchange_rate'=>$currency->exchange_rate]);}
-            else{$currency->save();}
+            else {
+                $currency->save();}
             
+        }} catch(\Exception $e) {
+            throw new HttpException(500, $e->getMessage());
         }
+        
+
         return "DATABASE HAS BEEN UPDATED WITH NEW EXCHANGE RATES";
     }
 }
